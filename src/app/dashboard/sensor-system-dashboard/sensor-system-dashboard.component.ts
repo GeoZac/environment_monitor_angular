8a73cb83e96ee3a86923c6f2e325b7ff46adbfb3
George Zacharia <george.zcharia@gmail.com>
2024-08-15 21:51:26 +0530

    src: Setup a dashboard module

---
+++ src/app/dashboard/sensor-system-dashboard/sensor-system-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sensor-system-dashboard',
+  templateUrl: './sensor-system-dashboard.component.html',
+  styleUrls: ['./sensor-system-dashboard.component.css']
+})
+export class SensorSystemDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
