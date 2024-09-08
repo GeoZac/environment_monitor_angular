ffe604b65ef281b8c074918bb672e620d966a8cf
George Zacharia <george.zcharia@gmail.com>
2024-09-08 10:51:59 +0530

    src: app: services: login: login.service: Implement methods
    - Login method
    - Version info method

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+  ) {
+    this.apiUrl = environment.baseUrl;
+  }
+
+  apiUrl: string;
+
+  readonly loginEndpoint = '/auth/login';
+  readonly versionEndpoint = '/public/status/version'
+
+  login(username: string, password: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    const bodyData = {
+      username: username,
+      password: password,
+    };
+
+    return this.httpClient.post(
+      `${this.apiUrl}${this.loginEndpoint}`,
+      bodyData,
+      { headers },
+    );
+  }
+
+  getAppVersion() {
+    return this.httpClient.get(
+      `${this.apiUrl}${this.versionEndpoint}`,
+      { responseType: 'text' },
+    );
+  }
 }
