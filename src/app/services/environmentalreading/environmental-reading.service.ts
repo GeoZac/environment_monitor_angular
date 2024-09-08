2ddfc351afd383746e00449bf7e9c4a7a0b1f564
George Zacharia <george.zcharia@gmail.com>
2024-09-08 10:47:44 +0530

    src: app: services: environmentalreading: environmental-reading.service: Implement methods

--- src/app/services/environmentalreading/environmental-reading.service.ts
+++ src/app/services/environmentalreading/environmental-reading.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnvironmentalReadingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.baseUrl + this.serviceEndpoint;
+  }
+
+  apiUrl: string;
+  serviceEndpoint: string = '/EnvironmentalReading';
+
+  getAllEnvironmentalReadingsBySensorSystemId(
+    sensorSystemId: string,
+    pageNo: number = 0,
+    pageSize: number = 10,
+    sortBy: string = 'id',
+    sortDir: string = 'asc',
+  ): Observable<any> {
+    let params = new HttpParams()
+      .set('pageNo', pageNo.toString())
+      .set('pageSize', pageSize.toString())
+      .set('sortBy', sortBy)
+      .set('sortDir', sortDir);
+
+    return this.http.get<any>(`${this.apiUrl}/SensorSystem/${sensorSystemId}`, {
+      params: params,
+    });
+  }
 }
