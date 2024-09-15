ecbdf622510dd852a003d343389cdcb57a6d9e37
George Zacharia <george.zcharia@gmail.com>
2024-09-15 20:16:45 +0530

    app: services: auth: auth.service: Implement methods

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -4,6 +4,54 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly TOKEN_KEY: string = 'access_token';
+  private readonly EXPIRY_KEY: string = 'token_expiry';
+  private readonly USER_INFO_KEY: string = 'user_info';
+
+  private accessToken: string | null = null;
+  private userInfo: any = null;
+
+  getAuthData() {
+    const token = localStorage.getItem(this.TOKEN_KEY);
+    const expiryTime = localStorage.getItem(this.EXPIRY_KEY);
+    const userInfo = localStorage.getItem(this.USER_INFO_KEY);
+
+    if (token && expiryTime) {
+      if (this.isTokenExpired(parseInt(expiryTime))) {
+        this.clearAuthData();
+        return { token: null, isExpired: true };
+      }
+      return {
+        token,
+        isExpired: false,
+        userInfo: JSON.parse(userInfo || '{}'),
+      };
+    }
+    return {
+      token: this.accessToken,
+      userInfo: this.userInfo,
+      isExpired: false,
+    };
+  }
 
   constructor() { }
+
+  // Store token and user info in memory
+  storeAuthData(token: string, userInfo: any) {
+    this.accessToken = token;
+    this.userInfo = userInfo;
+
+    const expiryTime = new Date().getTime() + 3600 * 1000; // expiresIn is in seconds
+    localStorage.setItem(this.TOKEN_KEY, token);
+    localStorage.setItem(this.EXPIRY_KEY, expiryTime.toString());
+    localStorage.setItem(this.USER_INFO_KEY, JSON.stringify(userInfo));
+  }
+
+  private isTokenExpired(expiryTime: number): boolean {
+    return new Date().getTime() > expiryTime;
+  }
+  clearAuthData() {
+    this.accessToken = null;
+    this.userInfo = null;
+  }
 }
