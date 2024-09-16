423354fa861606dce658e7070c3611592e57be79
George Zacharia <george.zcharia@gmail.com>
2024-09-16 19:15:20 +0530

    app: utils: auth.interceptor: Implement initial logic

--- src/app/utils/auth.interceptor.ts
+++ src/app/utils/auth.interceptor.ts
@@ -8,13 +8,27 @@ import {
     HttpEvent,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-    constructor() { }
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        throw new Error('Method not implemented.');
+    constructor(private authService: AuthService) { }
+
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler,
+    ): Observable<HttpEvent<any>> {
+        const authData = this.authService.getAuthData();
+
+        if (authData.token && !authData.isExpired) {
+            const clonedReq = req.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${authData.token}`,
+                },
+            });
+            return next.handle(clonedReq);
+        } else {
+            return next.handle(req);
+        }
     }
-
-
 }
